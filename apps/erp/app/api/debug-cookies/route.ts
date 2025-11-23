4b1c0898b07166aff99d2c92425a22cc9c9f0faf
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2025-11-23 23:00:28 +0700

    feat: update debug cookies to set test cookie

--- apps/erp/app/api/debug-cookies/route.ts
+++ apps/erp/app/api/debug-cookies/route.ts
@@ -2,7 +2,18 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function GET(request: NextRequest) {
-    return NextResponse.json({
+    const response = NextResponse.json({
         cookies: request.cookies.getAll(),
     });
+
+    // Try to set a test cookie
+    response.cookies.set("test-cookie", "hello", {
+        domain: ".personalapp.id",
+        path: "/",
+        secure: true,
+        sameSite: "none",
+        httpOnly: true,
+    });
+
+    return response;
 }
